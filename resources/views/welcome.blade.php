a830c17b7cb20d9ee47cf375fbf6f153130ebc4a
Kaung Khant <kaungkhant@Kaungs-MacBook-Air.local>
2022-10-13 23:33:58 +0630

    SocialApp

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Socialite App</title>
+    <link
+  href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/5.0.0/mdb.min.css"
+  rel="stylesheet"
+/>
+<style>
+    .wrapper{
+        margin: 300px auto;
+        margin-left: 40%;
+       
+    }
+</style>
+</head>
+<body class="">
+    <div class="wrapper">
+        <a href="/auth/redirect" class="btn btn-lg bg-danger text-white">Login With GitHub</a>
+    </div>
+    
+</body>
+</html>
